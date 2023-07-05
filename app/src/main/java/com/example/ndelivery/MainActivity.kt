6bfd8a592d3878cd2c92e24aa881afd15fe247f9
Cainã Oliveira <csoliveira01@gmail.com>
2023-07-05 19:23:58 -0400

    Updating product item

--- app/src/main/java/com/example/ndelivery/MainActivity.kt
+++ app/src/main/java/com/example/ndelivery/MainActivity.kt
@@ -7,20 +7,30 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
@@ -52,52 +62,69 @@ fun ProductItemPreview() {
 
 @Composable
 fun ProductItem() {
-    Column(
-        Modifier
-            .width(200.dp)
-            .height(250.dp)
+    Surface(
+        shape = RoundedCornerShape(15.dp),
     ) {
-        Box(
-            Modifier
-                .width(200.dp)
-                .height(100.dp)
-                .background(Color.Green)
-        )
-        {
-
-            Image(
-                modifier = Modifier
-                    .offset(x = 50.dp, y = 50.dp)
-                    .width(100.dp)
-                    .height(100.dp),
-                painter = painterResource(R.drawable.ic_launcher_background),
-                contentDescription = null
-            )
-        }
-
         Column(
             Modifier
-                .padding(horizontal = 16.dp)
-                .offset(y = 66.dp)
+                .width(200.dp)
+                .heightIn(250.dp, 280.dp)
         ) {
 
-            Text(
-                modifier = Modifier
-                    .width(168.dp)
-                    .height(48.dp),
-                text = "Lorem ipsum is placeholder text...",
-                fontSize = 18.sp
-            )
+            val imageSize = 100.dp
 
-            Text(
-                modifier = Modifier
-                    .width(59.dp)
-                    .height(17.dp),
-                text = "R$ 14,99",
-                fontSize = 14.sp,
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Color(0xFF6200EE),
+                                Color(0xFF03DAC5)
+                            )
+                        )
+                    )
             )
+            {
+
+                Image(
+                    modifier = Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize / 2)
+                        .align(Alignment.BottomCenter)
+                        .clip(shape = CircleShape),
+                    painter = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = null
+                )
+            }
+
+            Spacer(modifier = Modifier.height(imageSize / 2))
+
+            Column(
+                Modifier
+                    .padding(16.dp)
+            ) {
+
+                Text(
+                    modifier = Modifier
+                        .height(48.dp),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight(700),
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp
+                )
+
+                Text(
+                    modifier = Modifier
+                        .padding(top = 8.dp),
+                    text = "R$ 14,99",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
         }
-
     }
 }
 
