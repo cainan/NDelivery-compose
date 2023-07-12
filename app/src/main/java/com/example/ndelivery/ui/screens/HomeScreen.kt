73fdf5c0eedc80a6d75106a13cce9d3fb17eb1a2
Cainã Oliveira <csoliveira01@gmail.com>
2023-07-12 18:22:12 -0400

    Adding TextField

--- app/src/main/java/com/example/ndelivery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/ndelivery/ui/screens/HomeScreen.kt
@@ -1,11 +1,20 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.ndelivery.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,25 +23,34 @@ import com.example.ndelivery.sampledata.sampleSections
 import com.example.ndelivery.ui.components.ProductSection
 import com.example.ndelivery.ui.theme.NDeliveryTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(vertical = 16.dp)
-    ) {
+    Column()
+    {
+        var text by remember { mutableStateOf("") }
+        OutlinedTextField(value = text, onValueChange = {
+            text = it
+        })
 
-        for (section in sections) {
-            val title = section.key
-            val products = section.value
-            item {
-                ProductSection(
-                    title = title,
-                    products = products
-                )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
+
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductSection(
+                        title = title,
+                        products = products
+                    )
+                }
             }
         }
     }
