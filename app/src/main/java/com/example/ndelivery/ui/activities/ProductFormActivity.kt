4a258c7026ab6b10dc8ce7541a43515ea68a38e1
Cainã Oliveira <csoliveira01@gmail.com>
2023-07-21 19:50:59 -0400

    Creating image preview and formatting layout and keyboard

--- app/src/main/java/com/example/ndelivery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/example/ndelivery/ui/activities/ProductFormActivity.kt
@@ -6,10 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -22,12 +27,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.ndelivery.R
 import com.example.ndelivery.model.Product
 import com.example.ndelivery.ui.theme.NDeliveryTheme
 import java.math.BigDecimal
+import java.text.DecimalFormat
 
 class ProductFormActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,10 +67,13 @@ fun ProductFormScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(16.dp),
 
         ) {
+        Spacer(modifier = Modifier)
+
         Text(
             text = "Criando o produto",
             modifier = Modifier.fillMaxWidth(),
@@ -65,11 +81,34 @@ fun ProductFormScreen() {
         )
 
         var url by remember { mutableStateOf("") }
-        TextField(value = url, modifier = Modifier.fillMaxWidth(), label = {
-            Text("Url da imagem")
-        }, onValueChange = {
-            url = it
-        })
+
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(R.drawable.placeholder),
+                error = painterResource(R.drawable.placeholder),
+                contentDescription = null,
+            )
+        }
+
+        TextField(
+            value = url,
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text("Url da imagem")
+            },
+            onValueChange = {
+                url = it
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                imeAction = ImeAction.Next
+            )
+        )
 
         var name by remember { mutableStateOf("") }
         TextField(
@@ -80,10 +119,16 @@ fun ProductFormScreen() {
             },
             onValueChange = {
                 name = it
-            }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+                capitalization = KeyboardCapitalization.Words
+            )
         )
 
         var price by remember { mutableStateOf("") }
+        var formatter = remember { DecimalFormat("#.##") }
         TextField(
             value = price,
             modifier = Modifier.fillMaxWidth(),
@@ -91,8 +136,18 @@ fun ProductFormScreen() {
                 Text("Preço")
             },
             onValueChange = {
-                price = it
-            }
+                try {
+                    price = formatter.format(BigDecimal(it))
+                } catch (e: IllegalArgumentException) {
+                    if (it.isBlank()) {
+                        price = it
+                    }
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next
+            )
         )
 
         var description by remember { mutableStateOf("") }
@@ -106,7 +161,11 @@ fun ProductFormScreen() {
             },
             onValueChange = {
                 description = it
-            }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Sentences
+            )
         )
 
         Button(
@@ -130,9 +189,8 @@ fun ProductFormScreen() {
             Text("Salvar")
         }
 
+        Spacer(modifier = Modifier)
     }
-
-
 }
 
 @Preview(showSystemUi = true)
